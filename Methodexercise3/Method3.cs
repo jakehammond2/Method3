b7fd80b24d97ae2fbc0f6ef62f20492e0f897e31
Jake Hammond <jakehammond226@gmail.com>
2022-07-05 11:19:04 -0500

    Method3

---
+++ Methodexercise3/Method3.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Methodexercise3
+{
+    public class Method3
+    {
+        public static int Add(params int[] values)
+        {
+            int sum = 0;
+
+            foreach (int num in values)
+            {
+
+                sum += num;
+            }
+
+            return sum;         
+
+
+
+        }
+       
+    }
+}
+
---
+++ Methodexercise3/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+ using Methodexercise3;
+
+// var sum = Method3.Add;
+
+// Console.WriteLine(sum); 
+
+Console.WriteLine(Method3.Add(2, 3, 5, 7));
+
+Console.WriteLine(Method3.Add(2, 4, 5, 7, 4, 23, 17)); 
